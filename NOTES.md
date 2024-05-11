6474a7eec02839357298efdede95ffedd4a290c1
Mya Macke <147437865+Nebula-Mya@users.noreply.github.com>
2024-05-11 15:13:33 -0400

    Implement card attack and death functionality

--- NOTES.md
+++ NOTES.md
@@ -1,2 +1,31 @@
 # Path from /root
 /mnt/c/Users/MaWmM/Documents/GitHub/final-project-Nebula-Mya
+
+# notes
+> sort field objects by zone  
+>
+> cards attack from left to right
+>
+> after cards attack, check if card.status == 'dead', if so, call card.die(zone) (for sigils) and move it to graveyard object
+>
+>> BEFORE MOVING TO GRAVEYARD, check if card.status == 'undead', if so, move it to the hand
+>
+>> also replace it with a blank card with same zone  
+>>
+>> when playing a card, delete the current blank card in that zone, call card.play(zone), and move it to the field object
+>> 
+> field object should use a dictionary, with keys being zones  
+>
+>> maybe one field object with sub dictionaries for rows?  
+>>
+>> on turn end, re order the field according to zones
+>> 
+> graveyard should be viewable to player 
+>
+>> should be a list for ordering 
+>
+> at beginning of turn, check if decklist length is <= 0, if so, player loses  
+>
+> slime mold sigil is names 'split'
+>
+> need extra blank cards on the sides to prevent crashes, they wont get drawn (zones 0 and 6)
--- card.py
+++ card.py
@@ -1,3 +1,5 @@
+import sigils
+
 class BlankCard() :
     '''
     An empty card, serves both as an empty zone and as the parent class for all other cards
@@ -10,21 +12,22 @@ class BlankCard() :
         sigil : the sigil the card currently has (str)
         sigil_text : the three lines of ASCII art for the sigil's symbol (str)
         is_poisoned : whether the card is poisoned (bool)
+        status : whether the card is alive or dead (str)
         zone : the zone the card is in, with 0 as default (int)
 
     Methods :
         resetStats() : sets current stats to base stats
-        attack(left_card, front_card, right_card) : attacks zone(s) in front (in progress)
+        attack(front_left_card, front_card, front_right_card, left_card, right_card) : attacks zone(s) in front
         print() : prints self.name
         displayFull() : prints full card
         displayByLine() : prints one line for each call
-        takeDamage(damage) : reduces current life by damage 
-        play(zone) : plays card onto field (unimplemented)
-        die(zone) : replaces self with BlankCard (unimplemented)
+        takeDamage(damage) : reduces current life by damage (in progress)
+        play(zone) : activates sigils on entering field, resets stats, and updates zone
+        die(zone, left_card, right_card) : activates sigils on death and resets stats (in progress)
         explain() : prints explanation of stats and sigil for player (unimplemented)
         updateASCII() : updates the ASCII art for the card
     '''
-    def __init__(self, name = '      ', cost = ' ', attack = ' ', life = ' ', sigil = '', sigil_text = ['     ','     ','     ']) :
+    def __init__(self, name = '      ', cost = 0, attack = 0, life = 0, sigil = '', sigil_text = ['     ','     ','     '], status = 'alive', zone = 0) :
         self.name = name.ljust(9)[:9]
         self.cost = str("C:" + str(cost))
         self.base_attack = attack
@@ -34,7 +37,8 @@ class BlankCard() :
         self.sigil = sigil
         self.sigil_icon = sigil_text
         self.is_poisoned = False
-        self.zone = 0
+        self.status = status
+        self.zone = zone
         if name == '      ' :
             self.cost = '   '
             self.stats = '   '
@@ -61,17 +65,38 @@ class BlankCard() :
         self.current_life = self.base_life
         self.is_poisoned = False
         self.zone = 0
+        self.status = 'alive'
         self.updateASCII()
 
-    def attack(self, front_left_card, front_card, front_right_card) :
+    def attack(self, front_left_card, front_card, front_right_card, left_card, right_card) :
         # if sigil is;
-            # bifurcate: attacks front_left_card and front_right_card
-            # lane shift right: attacks front, then moves a lane right if possible
-                # if right_card is blank and self.zone isn't 5, move to the right
-            # lane shift left: attacks front, then moves a lane left if possible
-                # if left_card is blank and self.zone isn't 1, move to the left
-            # venom: attacks front, then poisons front
-            # if sigil is blank, attack front_card
+        ## blank: attack front_card
+        if self.sigil == '' :
+            front_card.takeDamage(self.current_attack)
+        ## bifurcate: attacks front_left_card and front_right_card
+        elif self.sigil == 'bifurcate' :
+            if front_left_card != None :
+                front_left_card.takeDamage(self.current_attack)
+            if front_right_card != None :
+                front_right_card.takeDamage(self.current_attack)
+        ## lane shift right: attacks front, then moves a lane right if possible
+            # if right_card is blank and self.zone isn't 5, move to the right
+        elif self.sigil == 'lane shift right' :
+            front_card.takeDamage(self.current_attack)
+            if (self.zone != 5) and (right_card.name == '      ') :
+                self.zone += 1
+                right_card.zone -= 1
+        ## lane shift left: attacks front, then moves a lane left if possible
+            # if left_card is blank and self.zone isn't 1, move to the left
+        elif self.sigil == 'lane shift left' :
+            front_card.takeDamage(self.current_attack)
+            if (self.zone != 1) and (left_card.name == '      ') :
+                self.zone -= 1
+                left_card.zone += 1
+        ## venom: attacks front, then poisons front
+        elif self.sigil == 'venom' :
+            front_card.takeDamage(self.current_attack)
+            front_card.is_poisoned = True
         # if poisoned, deal 1 damage to self
         if self.is_poisoned :
             self.takeDamage(1)
@@ -89,16 +114,42 @@ class BlankCard() :
         return self.text_lines[self.line_cursor - 1]
 
     def takeDamage(self, damage) :
-        self.current_life -= damage
-        self.updateASCII()
-        if self.current_life <= 0 :
-            self.die(self.zone)
+        if self.name == '      ' or self.status == 'dead':
+            # deal damage to opposite player
+            pass
+        else :
+            self.current_life -= damage
+            self.updateASCII()
+            if self.current_life <= 0 :
+                self.status = 'dead'
 
     def play(self, zone) :
-        pass
+        if self.sigil == '' :
+            self.resetStats()
+            self.zone = zone
+            self.updateASCII()
 
-    def die(self, zone) :
-        pass
+    def die(self, zone, left_card, right_card) :
+        if self.sigil == '' :
+            self.resetStats()
+            self.updateASCII()
+        elif self.sigil == 'split' :
+            self.resetStats()
+            self.updateASCII()
+            if self.base_life > 1 and self.base_attack > 1 :
+                if left_card.name == '     ' and self.zone != 1 :
+                    # play a copy of self with halved stats and no sigil to the left
+                    pass
+                if right_card.name == '     ' and self.zone != 5 :
+                    # play a copy of self with halved stats and no sigil to the right
+                    pass
+        elif self.sigil == 'unkillable' :
+            if self.name == 'Ouroboros' :
+                self.base_attack += 1
+                self.base_life += 1
+            self.resetStats()
+            self.status = 'undead'
+            self.updateASCII()
 
     def explain(self) :
         pass
