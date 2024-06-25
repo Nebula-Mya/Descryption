a03dfb6cc7800212aa480059c39af412868e981b
Mya Macke <147437865+Nebula-Mya@users.noreply.github.com>
2024-06-25 16:26:50 -0400

    chore: Refactor sigil handling in deck.py

--- deck.py
+++ deck.py
@@ -61,31 +61,35 @@ class Deck() :
             sigil: sigil to change to (str)
             sigil_slot: slot to change sigil in, 1 or 2 (int)
         '''
-
-        # imports
-        import sigils
-
         # set up variables
         sorted_deck = QoL.sort_deck(self.cards)
-
-        # error handling
-        self.check_index(index)
-        try :
-            same_sigil = sorted_deck[index].sigil[0] == sorted_deck[index].sigil[1]
-        except IndexError :
-            same_sigil = False
-        try :
-            bad_pair = sorted_deck[index].sigil[0] in sigils.movers and sorted_deck[index].sigil[1] in sigils.movers
-        except IndexError :
-            bad_pair = False
-        if same_sigil or bad_pair :
-            raise ValueError(f"invalid sigil pair: {sorted_deck[index].sigil[0]} and {sorted_deck[index].sigil[1]}")
-        if sigil_slot not in [1, 2] :
-            raise ValueError(f"invalid sigil slot: {sigil_slot}")
+        same_sigil = False
 
         # if first sigil is empty, always change it
         if sorted_deck[index].sigil[0] == '' :
             sigil_slot = 1
+
+        # error handling
+        self.check_index(index)
+        if sigil_slot not in [1, 2] :
+            raise ValueError(f"invalid sigil slot: {sigil_slot}")
+        if len(sorted_deck[index].sigil) == 2 :
+            match sigil_slot :
+                case 1 :
+                    same_sigil = (sorted_deck[index].sigil[1] == sigil) or ('hefty' in sorted_deck[index].sigil[1] and 'hefty' in sigil)
+                case 2 :
+                    same_sigil = (sorted_deck[index].sigil[0] == sigil) or ('hefty' in sorted_deck[index].sigil[0] and 'hefty' in sigil)
+        elif sigil_slot == 2 :
+            same_sigil = (sorted_deck[index].sigil[0] == sigil) or ('hefty' in sorted_deck[index].sigil[0] and 'hefty' in sigil)
+        if same_sigil :
+            match sigil_slot :
+                case 2 :
+                    remaining_sigil = sorted_deck[index].sigil[0]
+                case _ if len(sorted_deck[index].sigil) == 2 :
+                    remaining_sigil = sorted_deck[index].sigil[1]
+                case _ :
+                    remaining_sigil = ''
+            raise ValueError(f"invalid sigil pair: {remaining_sigil} and {sigil}")
         
         # change sigil
         match sigil_slot :
@@ -96,9 +100,7 @@ class Deck() :
                     sorted_deck[index].sigil.append('') # if only one sigil is given, add empty string
                 sorted_deck[index].sigil[1] = sigil
         
-        # sorted_deck[index].update_ASCII()
-        for card in self.cards :
-            card.update_ASCII()
+        sorted_deck[index].update_ASCII()
 
     def shuffle(self) :
         '''
